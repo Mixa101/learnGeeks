85f3be8f0af5e0a340d754fdb4564544db29890f
Islam <kirolkuro@gmail.com>
2024-03-01 10:54:20 +0600

    fixed bugs

--- hero.py
+++ hero.py
@@ -8,7 +8,7 @@ class SuperHero:
         self.catchphrase = catchphrase
     def get_name_hero(self):
         return self.name
-    def multiplication_hp(self):qweqw
+    def multiplication_hp(self):
         self.health_points = self.health_points * 2
         print(f'succesful multiplication health point\'s to {self.health_points}')
     
@@ -18,4 +18,4 @@ class SuperHero:
 zoro = SuperHero('Zoro', 'king of hell', 'three sword style', 50, 'where i am, give me bear')
 print(zoro)
 print(zoro.get_name_hero())
-zoro.multiplication_hp()qweqw
+zoro.multiplication_hp()
